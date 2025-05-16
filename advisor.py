6d16870312123ca78ef96735fddc7bc9cd07fc03
Elizaveta Anikina <124817122+lizokkolobok@users.noreply.github.com>
2025-05-16 16:12:19 +0300

    Update advisor.py

--- advisor.py
+++ advisor.py
@@ -1,8 +1,11 @@
 import openai
 import os
 from mistral_inference import get_mistral_response
+from gigachat_inference import get_gigachat_response
 
-openai.api_key = ""
+
+openai.api_key = os.getenv("OPENAI_API_KEY") or ""
+
 
 def get_advice(prompt, model="gpt-4o", mode="completion"):
     if model.startswith("gpt"):
@@ -16,13 +19,14 @@ def get_advice(prompt, model="gpt-4o", mode="completion"):
         else:
             response = openai.ChatCompletion.create(
                 model=model,
-                messages=[
-                    {"role": "user", "content": prompt}
-                ],
+                messages=[{"role": "user", "content": prompt}],
                 temperature=0.7
             )
             return response.choices[0].message["content"].strip()
     elif model == "mistral":
         return get_mistral_response(prompt)
+    elif model == "gigachat":
+        return get_gigachat_response(prompt)
     else:
         return "Model not supported."
+
