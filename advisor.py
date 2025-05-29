a262d02ddb6df6e527ee49b40c18540de4d2744c
Elizaveta Anikina <124817122+lizokkolobok@users.noreply.github.com>
2025-05-29 20:20:14 +0300

    Update advisor.py

--- advisor.py
+++ advisor.py
@@ -4,7 +4,7 @@ from mistral_inference import get_mistral_response
 from gigachat_inference import get_gigachat_response
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY") or "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"
+openai.api_key = os.getenv("OPENAI_API_KEY") or ""
 
 
 def get_advice(prompt, model="gpt-4o", mode="completion"):
